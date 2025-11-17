b5271a6239d235e2823f8bd17da623fce725edb2
mizuno <newuser@mPC>
2025-11-17 10:05:05 +0900

    Fix pandas build issue for Render

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# Renderのビルド時に実行されるスクリプト
-
 set -o errexit
 
 # 依存関係のインストール
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir -r requirements.txt
+
+# FLASKアプリケーションのパスを指定
+export FLASK_APP=wsgi:app
 
 # データベースマイグレーション
 flask db upgrade
